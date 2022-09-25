128bd927b14b9776e70ef82a594b702a89d56e7d
Goddaybc <goddayoghenerobiz@gmail.com>
2022-09-25 14:03:44 -0700

    mutiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if lenth == 0:
+    if length == 0:
         result = (0, None)
         return result
     else:
