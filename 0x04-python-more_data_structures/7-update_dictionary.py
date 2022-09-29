d7bdd38ce7b23b4dc5aa827dbffef0d627720f10
Goddaybc <goddayoghenerobiz@gmail.com>
2022-09-29 04:22:42 -0700

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dict, key, value):
+    """
+    add or replace a new key value in a dict
+    """
+    if a_dict is None:
+        return None
+    a_dict[key] = value
+    return a_dict
