a447218baa3721381371e8a1f33c93829f86e332
Goddaybc <goddayoghenerobiz@gmail.com>
2022-09-29 04:50:42 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dict):
+    """
+    find the key with the highest value
+    """
+    if a_dict is None or a_dict == {}:
+        return None
+    best = max(a_dict.values())
+    for key in a_dict.keys():
+        if a_dict[key] == best:
+            return key
